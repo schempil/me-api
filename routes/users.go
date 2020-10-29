ce194f7f02412ef9d11af4a3a552393d2354cdd0
Philipp Schemel <philippschemel@live.com>
2020-10-29 18:02:11 +0100

    Keep it simple with one example

--- routes/users.go
+++ routes/users.go
@@ -12,10 +12,4 @@ func addUserRoutes(rg *gin.RouterGroup) {
 	users.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, "users")
 	})
-	users.GET("/comments", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "users comments")
-	})
-	users.GET("/pictures", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "users pictures")
-	})
 }
